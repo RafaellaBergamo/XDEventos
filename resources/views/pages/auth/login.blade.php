4f8f2081fb7215088b5977ebd769949719ae4372
Rafaella Bergamo <rafaellabergamo3@gmail.com>
2021-11-23 21:04:13 -0300

    Tentativa de token

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.master-mini')
 @section('content')
 <link rel="stylesheet" href="/css/forgotpassword.css"> 
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <div class="content-wrapper d-flex align-items-center justify-content-center auth theme-one" style="background-image: linear-gradient(to bottom right, rgba(191, 224, 242, 0.29), rgba(191, 224, 242, 0)), url('/assets/images/auth/background-login.png'); background-size: cover;">
   <div class="row w-100">
     <div class="col-lg-4 mx-auto">
       <div class="auto-form-wrapper">
       <h4 class="text-center mb-4">Login</h4>
         <form action="{{ route('login') }}" method="POST" class="needs-validation" novalidate>
-        {{ csrf_field() }}
           <div class="form-group">
             <label class="label">Email</label>
             <div class="input-group">
@@ -34,7 +34,6 @@
           <div class="text-block text-center my-3">
             <span class="text-small font-weight-semibold">Novo por aqui ?</span>
             <a href="{{ url('/auth/register') }}" class="text-black text-small">Crie sua conta</a>
-            <input type="hidden" id="token" value="{{ csrf_token() }}">
           </div>
         </form>
       </div>
